2f76d59e96fd6390dfeaf384fa53afd3c65e7a5f
masea3439 <mathieuseahlabbe@hotmail.com>
2019-06-29 13:09:03 -0400

    Added minimap

--- graphicshandler.py
+++ graphicshandler.py
@@ -3,23 +3,28 @@ from sprites import SpritesContainer
 from math import degrees
 width = 1440
 height = 900
+map_width = 1500
+map_height = 1500
 
 
 class GraphicsHandler:
 	def __init__(self):
 		self.x_offset = 0
 		self.y_offset = 0
-		self.sprites_container = SpritesContainer()
 		self.screen = pygame.display.set_mode((width, height))
 		pygame.display.set_caption("Tankagons")
+		self.sprites_container = SpritesContainer(self.screen)
+		self.map_screen = pygame.Surface((200, 200))
 
 	def update_display(self, data, x_offset, y_offset, trees):
 		self.screen.fill((60, 112, 45))
+		self.map_screen.fill((0, 0, 0))
 		self.x_offset = x_offset
 		self.y_offset = y_offset
 		self.draw_tanks(data["tanks"])
 		self.draw_bullets(data["bullets"])
 		self.draw_trees(trees)
+		self.screen.blit(self.map_screen, (width-200, 0))
 		pygame.display.update()
 
 	def draw_background(self):
@@ -29,6 +34,7 @@ class GraphicsHandler:
 		for tank in tank_data.values():
 			self.draw_tank_body(tank.x - self.x_offset + width//2, tank.y - self.y_offset + height//2, tank.body_rotation, tank.tank_body_model)
 			self.draw_tank_turret(tank.x - self.x_offset + width//2, tank.y - self.y_offset + height//2, tank.turret_rotation, tank.tank_turret_model)
+			pygame.draw.rect(self.map_screen, (255, 0, 0), (int(tank.x/map_width*200), int(tank.y/map_width*200), 5, 5))
 
 	def draw_bullets(self, bullet_data):
 		for bullet in bullet_data:
@@ -45,6 +51,7 @@ class GraphicsHandler:
 		for tree in trees.values():
 			if (leftmost <= tree.x <= rightmost) and (topmost <= tree.y <= bottommost):
 				pygame.draw.circle(self.screen, (145, 103, 70), (tree.x-leftmost, tree.y-topmost), tree.radius)
+			pygame.draw.circle(self.map_screen, (255, 0, 0), (int(tree.x / map_width * 200), int(tree.y / map_width * 200)), 2)
 
 	def draw_bullet(self, x: int, y: int, bullet_angle: int):
 		bullet_image = self.sprites_container.bullet_sprites['bullet']
